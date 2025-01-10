e1ae7fa6dd26b89d66e008148bc9b07b3b932b26
Name Surname <name_surname@1c.ru>
2025-01-10 23:47:53 +0300

    Bootstrap, cards and Google tags

--- flaskr/static/js/work-with-coockies.js
+++ flaskr/static/js/work-with-coockies.js
@@ -17,6 +17,6 @@ function getCookie(name) {
     }
     return null;
 }
-function eraseCookie(name) {   
+function eraseCookies(name) {   
     document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
