127bc0e2defcec5ce97485b5b2af82d8ca4de033
Name Surname <name_surname@1c.ru>
2025-01-19 19:57:38 +0300

    element view tuning

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -70,3 +70,38 @@ CREATE TABLE game_and_element (
   game_id INTEGER REFERENCES game (id) ON DELETE CASCADE,
   element_id INTEGER REFERENCES element (id) ON DELETE RESTRICT
 );
+
+DROP TABLE IF EXISTS quest;
+DROP TABLE IF EXISTS quest_task;
+
+CREATE TABLE quest (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  author_id INTEGER NOT NULL,
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  --
+  city TEXT NOT NULL,
+  title TEXT NOT NULL,
+  description TEXT,
+  static_link TEXT,
+  image_static_link TEXT,
+  price real DEFAULT 0.0,
+  start_point TEXT NOT NULL,
+  --
+  first_task_id INTEGER REFERENCES quest_task (id) ON DELETE SET NULL,
+  FOREIGN KEY (author_id) REFERENCES user (id)
+);
+
+CREATE TABLE quest_task (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  --
+  description TEXT NOT NULL,
+  image_static_link TEXT,
+  first_clue TEXT,
+  second_clue TEXT,
+  third_clue TEXT,
+  answer TEXT,
+  --
+  next_task_id INTEGER REFERENCES quest_task (id) ON DELETE SET NULL,
+  quest_id INTEGER NOT NULL REFERENCES quest (id) ON DELETE CASCADE
+);
