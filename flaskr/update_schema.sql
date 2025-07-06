28b416ce109398be5ac431f373a1e0ea18db4e7f
Name Surname <name_surname@1c.ru>
2025-07-06 22:31:47 +0300

    After the third  attempt to update db on production server.

--- flaskr/update_schema.sql
+++ flaskr/update_schema.sql
@@ -4,8 +4,8 @@
 --ALTER TABLE game_and_element
 --ADD COLUMN type_of_id TEXT NOT NULL DEFAULT 'element' CHECK (type_of_id IN ('element', 'game_element'));
 
-ALTER TABLE game_and_element
-ADD COLUMN game_element_id INTEGER REFERENCES game_and_element (id) ON DELETE CASCADE;
+--ALTER TABLE game_and_element
+--ADD COLUMN game_element_id INTEGER REFERENCES game_and_element (id) ON DELETE CASCADE;
 
 ALTER TABLE game_and_element
 ADD COLUMN description TEXT;
